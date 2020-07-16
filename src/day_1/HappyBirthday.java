3d9cd7539396ef3b7f5c7a054d9e147c66f7c6c0
hareg-202 <63565231+hareg-202@users.noreply.github.com>
2020-07-16 17:07:56 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,7 +4,7 @@ public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("today is my best friends birthday! ");
         
-        System.out.println("today is all B18 birthday, this is edited from github")
+        System.out.println("today is all B18 birthday, this is edited from github.");
         
     }
 }
