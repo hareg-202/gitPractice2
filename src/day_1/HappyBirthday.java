ba05242374e218d1f8229c304b65d045fce3fffb
hareg-202 <63565231+hareg-202@users.noreply.github.com>
2020-07-16 16:57:14 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,5 +3,7 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("today is my best friends birthday! ");
+        // hanna's birthday
+        
     }
 }
