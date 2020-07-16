9f299867234e72f0c8883452869cd29e8f557b3f
hareg-202 <63565231+hareg-202@users.noreply.github.com>
2020-07-16 17:37:45 -0400

    Update AmazingRace.java

--- src/day_1/AmazingRace.java
+++ src/day_1/AmazingRace.java
@@ -3,5 +3,8 @@ package day_1;
 public class AmazingRace {
     public static void main(String[] args) {
         System.out.println("A lot of people are registering for the upcoming marathon.");
+        
+        System.out.println("Hanna is one of the runners from this marathon, added from github.");
     }
+    
 }
