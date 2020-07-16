26440ffac68221aec58c0485cded64ac2d7f6d09
hareg-202 <63565231+hareg-202@users.noreply.github.com>
2020-07-15 21:28:58 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,5 +3,7 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("I hope everything is Good");
+        
+        // this line is added from github
     }
 }
