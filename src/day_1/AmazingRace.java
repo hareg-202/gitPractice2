4d00c3837238006281a4037a0b00f0c2659a1427
Haregewoin Nirea <haregewoin.nirea@gmail.com>
2020-07-16 17:35:18 -0400

    add AmazingRace.java

---
+++ src/day_1/AmazingRace.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class AmazingRace {
+    public static void main(String[] args) {
+        System.out.println("A lot of people are registering for the upcoming marathon.");
+    }
+}
