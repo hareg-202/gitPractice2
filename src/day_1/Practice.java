eeda56d92dacd3b080be61c512e84d1790147d5a
hareg-202 <63565231+hareg-202@users.noreply.github.com>
2020-07-15 21:43:01 -0400

    Update Practice.java

--- src/day_1/Practice.java
+++ src/day_1/Practice.java
@@ -3,5 +3,7 @@ package day_1;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("I am practicing pull and push codes");
+        
+        // hi from github
     }
 }
